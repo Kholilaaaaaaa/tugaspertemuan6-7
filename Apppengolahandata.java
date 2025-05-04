681307278781915dfdb3538bba7352865b17a502
UTS <keigosuherman99@gmai.com>
2025-05-04 23:18:53 +0700

    first commit

---
+++ Apppengolahandata.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package apppengolahandata;
+
+/**
+ *
+ * @author ACER
+ */
+public class Apppengolahandata {
+    public class Customer {
+    private String nama;
+    private String noHp;
+    private String alamat;
+
+    public Customer(String nama, String noHp, String alamat) {
+        this.nama = nama;
+        this.noHp = noHp;
+        this.alamat = alamat;
+    }
+
+    public String getNama() { return nama; }
+    public String getNoHp() { return noHp; }
+    public String getAlamat() { return alamat; }
+}
+
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      
+    }
+    
+}
